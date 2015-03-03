a9c8597ce1f2a82837e1102ccaf1a0736e7b105d
foxart78 <giuseppe.toscano78@gmail.com>
2015-03-03 19:46:31 +0100

    In questa versione sono state configurate le impostazioni di autenticazione. le tabelle relative all'autentiazione sono state spostate sul DB principale. Sono stati creati due ruoli sul Db User e Admin ed é stato creato un template nestato nel principale da utilizzare nelle pagine di amministrazione.

---
+++ ABCDemo2/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ABCDemo2.Controllers
+{
+    [Authorize(Roles="admin")]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Qualifiche()
+        {
+            return View();
+        }
+    }
+}
